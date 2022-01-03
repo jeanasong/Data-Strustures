a1385c2c74aa2db2d1f685667811367431f3a4b4
jeanasong <58866533+jeanasong@users.noreply.github.com>
2022-01-02 23:56:40 -0500

    Add files via upload
    Updating with comment

--- Data-Structures/HashMap.js
+++ Data-Structures/HashMap.js
@@ -8,9 +8,22 @@ class HashMap {
       for(let i = 0; i < key.length; i++) {
         hashCode += hashCode + key.charCodeAt(i);
       }
-      return hashCode;
+      /* compress the value stored in hashCode by using modular arithmetic 
+      to return the remainder of dividing hashCode by the length of the hash map.
+      */
+      return hashCode % this.hashmap.length;
     }
     
   }
   
   module.exports = HashMap;
+
+  /* 
+  Check your work. Save a new HashMap instance with a size of 3, 
+  in a constant myHashMap and use the new .hash() to log the result of hashing the key 'id'. Hash and log 'id' again. 
+  Are the logged values the same or are they different?
+  */
+
+  const myHashMap = new HashMap(3);
+  console.log(myHashMap.hash('id'));
+  console.log(myHashMap.hash('id'));
