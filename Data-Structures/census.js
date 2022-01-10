f61217d3fe7a61730eb1db3d1cfc5e70c59d1237
jeanasong <58866533+jeanasong@users.noreply.github.com>
2022-01-09 23:15:32 -0500

    Add files via upload
    Building a person a fully HashMap functions.

---
+++ Data-Structures/census.js
@@ -0,0 +1,26 @@
+// Reviewing and building a fully-functional hash map!
+
+const HashMap = require('./HashMap');
+
+/* Create a constant that stores a hash map, birdCensus. 
+Use the hash map data structure to store all bird sightings. 
+Give it an array size of 16. */
+
+const birdCensus = new HashMap(16);
+
+/* Assign the following key-value pairs to birdCensus:
+
+Key: 'mandarin duck', Value: 'Central Park Pond'
+Key: 'monk parakeet', Value: 'Brooklyn College'
+Key: 'horned owl', Value: 'Pelham Bay Park' */
+
+birdCensus.assign('mandarin duck', 'Central Park Pond');
+birdCensus.assign('monk parakeet', 'Brooklyn College');
+birdCensus.assign('horned owl', 'Pelham Bay Park');
+
+// Retrieve the location for each of the three birds counted in birdCensus. 
+// Log them to the terminal.
+
+console.log(birdCensus.retrieve('mandarin duck'));
+console.log(birdCensus.retrieve('monk parakeet'));
+console.log(birdCensus.retrieve('horned owl'));
