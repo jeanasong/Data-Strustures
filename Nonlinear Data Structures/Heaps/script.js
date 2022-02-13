58722ace2c308989c3ee35176d964c2714c90320
jeanasong <58866533+jeanasong@users.noreply.github.com>
2022-02-13 01:22:44 -0500

    Working with Heaps

---
+++ Nonlinear Data Structures/Heaps/script.js
@@ -0,0 +1,5 @@
+const MinHeap = require('./MinHeap');
+
+const minHeap = new MinHeap(); // make an instance of MinHeap and assign it to the const variable minHeap.
+
+console.log(minHeap.heap); // display the heap content of minHeap.
