4d41781c8e8f085ebd6304f2ad14d24428bdb1b8
jeanasong <jeanasong46@yahoo.com>
2022-03-17 23:54:49 -0400

    Building depthFirstTraversal project.

--- Graph Data Structures/Graph Traversals/depthFirstTraversal.js
+++ Graph Data Structures/Graph Traversals/depthFirstTraversal.js
@@ -1,6 +1,15 @@
 const testGraph = require('./testGraph.js');
 
 const depthFirstTraversal = () => {
+    console.log(start.data)
+  
+  if(start.edges.length) {
+    const neighbor = start.edges[0].end;
+    if(!visitedVertices.includes(neighbor)) {
+      visitedVertices.push(neighbor);
+      depthFirstTraversal(neighbor, visitedVertices);
+    }
+  }
 
 };
 
