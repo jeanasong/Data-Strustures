63ca01241cd96e5962628b3739ce8fda89705b2a
jeanasong <jeanasong46@yahoo.com>
2022-03-28 23:58:46 -0400

    Evaluate All Paths

--- Graph Data Structures/Dijkstra's Algorithm/dijkstras.js
+++ Graph Data Structures/Dijkstra's Algorithm/dijkstras.js
@@ -36,6 +36,18 @@ const dijkstras = (graph, startingVertex) => {
     After distances is created, create a variable, previous, and set it equal to an empty object. This will be a map of every vertex to its corresponding previous vertex. */
     const previous = {}; // After distances is created, create a variable, previous, and set it equal to an empty object. This will be a map of every vertex to its corresponding previous vertex.
 
+    const queue = new PriorityQueue(); // Right after the previous object is instantiated, create a const variable, queue, and set it to a new instance of a PriorityQueue.
+
+    // The first vertex we want to evaluate is the starting vertex. After instantiating the queue, make a call to .add() the starting vertex to the queue. Pass in an object with the vertex property set to the startingVertex, and the priority property set to 0. The priority is the vertex’s distance to the starting vertex.
+    queue.add({ vertex: startingVertex, priority: 0});
+
+    /*Currently, we are iterating through the startingVertex’s edges and calculating the distance of the alternate path using the startingVertex’s distance. Now, we want to shift to evaluate the vertices in the queue.
+
+Instead of setting the vertex variable to the starting vertex, we want to set it to the vertex with the smallest priority in the queue, which will initially be the starting vertex. Dequeue the vertex with the smallest priority from the queue by calling the .popMin() method and declare the vertex variable by destructuring it from the resulting object. */
+    const { vertex } = queue.popMin();
+
+
+
     graph.vertices.forEach((vertex) => { // Iterate through the graph’s vertices using .forEach()
         /*Before we can start evaluating the paths, we also need to initialize each vertex’s distance to Infinity. Any connection will be shorter than Infinity, no matter how large the weight is. 
         As long as there is a connection between two vertices, the connection will always be recognized as part of the shortest path over Infinity.
@@ -50,6 +62,11 @@ const dijkstras = (graph, startingVertex) => {
     /*Now, you may be wondering, “but the distance from the starting vertex to the starting vertex is 0, not Infinity!” Let’s resolve this by adjusting the initial distance for the starting vertex.
     After we finish iterating through the graph’s vertices, set the distance of the startingVertex.data in distances to 0. */
     distances[startingVertex.data] = 0;
+
+    /*Great! Our queue is running along, so we just need to set up a loop to go through all of the vertices in the queue. As long as there are vertices left in the queue, we should continue evaluating alternate paths.
+
+After the distance of startingVertex is set to 0, set up a while loop that continues to evaluate the distances as long as the queue is not empty. You can call .isEmpty() on queue to check if it is empty or not. This should come right before the vertices are popped from the queue and end right after we iterate through the neighbors. */
+    while(!queue.isEmpty()) {
 
     /* Evaluate Paths to Starting Vertex’s Neighbors
     We have all of our records set up, so we can start traversing through the graph and evaluating the distances from the starting vertex to its neighbors.
@@ -71,6 +88,11 @@ const dijkstras = (graph, startingVertex) => {
             distances[neighborValue] = alternate; // If the alternate path is shorter, set distances at the neighborValue to the new alternate cost. We also want to set the previous vertex at the neighborValue to vertex.
             previous[neighborValue] = vertex; // We also want to set the previous vertex at the neighborValue to vertex.
 
+            /*For now, we only have the startingVertex queued up for evaluation. However, we also want to queue up any neighbors where a shorter distance from the vertex to the neighbor is found. This is because other paths that go through this neighbor could be shorter than what was previously recorded.
+
+            If the alternate path to the neighbor is shorter than the previously recorded distance, .add() the vertex’s neighbor to the queue where the vertex property is set to the neighbor, and the priority is the new neighborValue in distances. */
+            queue.add({vertex: edge.end, priority: distances[neighborValue]})
+
             /* When we evaluate the distances, we are determining if the path from the starting vertex to the neighbor is shorter than the previously evaluated distance. 
             Since we have not evaluated any paths to the neighbors yet, the previously recorded distances to all of the neighbors is Infinity.
             Run the code and look at the output of the function. The shortest paths evaluated so far should be the paths from the starting vertex to its neighbors. 
@@ -78,6 +100,7 @@ const dijkstras = (graph, startingVertex) => {
             In previous, we should see the neighbors with the starting vertex as their previous vertex, and all other vertices with null. */
         }
     })
+}
 
     /* Great! Now we want to return the results of our evaluations in distances and previous. This allows for the user who makes a call to our function to use these computed values.
     You can return both values by returning an object with distances and previous keys set to their respective variables.*/
